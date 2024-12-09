529c9af93392e7552126a6a34d89fea49c7d679d
Bothriopsis <fisterniklas@outlook.com>
2024-12-09 09:39:25 +0100

    first run

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from gameObjects.moving import movingBar
 from gameObjects.ball import bounciBall
+from gameObjects.grid import Grid
 
 class Game:
     pygame.init()
@@ -11,4 +12,25 @@ class Game:
 
     bar = movingBar()
     ball = bounciBall()
-
+    grid = Grid()
+    def __init__(self):
+        self.game = True
+    
+    def run(self):
+        while self.game:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.game = False
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                self.bar.move(-10)
+            if keys[pygame.K_RIGHT]:
+                self.bar.move(10)
+            self.ball.move()
+            self.screen.fill((0,0,0))
+            self.bar.draw(self.screen)
+            self.ball.draw(self.screen)
+            self.grid.draw(self.screen)
+            pygame.display.flip()
+            self.clock.tick(60)
+        pygame.quit()

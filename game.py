bd3cc653b5fdbd005bd045d6d86382e7bf929653
Bothriopsis <fisterniklas@outlook.com>
2024-12-09 16:44:19 +0100

    grid is here

--- game.py
+++ game.py
@@ -2,12 +2,13 @@ import pygame
 from gameObjects.moving import movingBar
 from gameObjects.ball import bounciBall
 from gameObjects.grid import Grid
+from gameObjects.grid import Rect
 from gameObjects.background import Background
 
 class Game:
     pygame.init()
-    SCREEN_WIDTH = 1280
-    SCREEN_HEIGHT = 720
+    SCREEN_WIDTH = pygame.display.Info().current_w
+    SCREEN_HEIGHT = pygame.display.Info().current_h
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
@@ -25,17 +26,17 @@ class Game:
                     if event.type == pygame.QUIT:
                         self.game = False
                 keys = pygame.key.get_pressed()
-                if keys[pygame.K_LEFT]:
+                if keys[pygame.K_LEFT] or keys[pygame.K_a]:
                     self.bar.move(-10)
-                if keys[pygame.K_RIGHT]:
+                if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                     self.bar.move(10)
                 self.ball.move()
                 self.background.draw(self.screen)
                 self.bar.draw(self.screen)
                 self.ball.draw(self.screen)
                 self.ball.collide(self.bar)
-                for i in Grid.all_grids():
-                    self.grid.make(self.screen)
+                for i in Rect.all_grids():
+                    pygame.draw.rect(self.screen, "grey", i)
                 pygame.display.flip()
                 self.clock.tick(165)
             
--- gameObjects/grid.py
+++ gameObjects/grid.py
@@ -5,30 +5,26 @@ class Rect:
     def __init__(self, x, y):
         SCREEN_WIDTH = pygame.display.Info().current_w
         SCREEN_HEIGHT = pygame.display.Info().current_h
-        self.x = x
-        self.y = y
         self.position = (x*(SCREEN_WIDTH/30)+(SCREEN_WIDTH/6), y*(SCREEN_WIDTH/30)+(SCREEN_HEIGHT/15))
         self.color = "grey"
 
-    def make(self, screen):
+    def make(self):
         SCREEN_WIDTH = pygame.display.Info().current_w
         self.rect = pygame.Rect(self.position[0], self.position[1], 0.9*(SCREEN_WIDTH/30), 0.9*(SCREEN_WIDTH/30))
         Rect.all.append(self.rect)
-        pygame.draw.rect(screen, self.color, self.rect)
+
+    def collide(self, player):
+        if self.rect.colliderect(player.rect):
+            return True
+        player.dx = -player.dx
+        player.dy = -player.dy
 
     @classmethod
     def all_grids(cls):
         return Rect.all
 
-class Grid(Rect):
+class Grid:
     def __init__(self, screen):
         for i in range(20):
             for j in range(10):
-                super().__init__(i, j) 
-    
-    def canCollide(self, other):
-        for i in self.grid:
-            if i.position[0] == other.position[0]:
-                self.grid.remove(i)
-            if i.position[1] == other.position[1]:
-                self.grid.remove(i)
+                Rect(i, j).make()
